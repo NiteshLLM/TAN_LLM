997f57563c5abb0522781efbd114416782826215
NiteshGit123 <niteshdatapkd@gmail.com>
2024-09-13 22:47:56 +0530

    modified app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Hello Streamlit!")
 st.write("This is a simple Streamlit app.")
-st.write("You can deploy it on Streamlit Cloud!")
+
 
 # Add an input box and a button
 name = st.text_input("Enter your name:")
